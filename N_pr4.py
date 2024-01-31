2e06b54efacc10ec90f3a83e5835319b7156e364
Gayanga Dulanjana <158056564+Gayangadw@users.noreply.github.com>
2024-01-31 11:14:51 +0530

    N_pr4.py

---
+++ N_pr4.py
@@ -0,0 +1,14 @@
+# calculator creating using tkinter
+from tkinter import Tk, Entry, Button, StringVar
+class calculator :
+    def __int__(self,master):
+     master.title("Calculator")
+     master.geometry("357x420+0+0")
+     master.config(bg='gray')
+     master.resizable(False,False)
+
+     self.eqation=stringvar()
+
+     root=Tk()\
+   root.mainloop()
+
