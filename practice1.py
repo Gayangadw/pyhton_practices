d11de9ef1ee6f659713606b118cf9e32c7f10765
Gayanga Dulanjana <158056564+Gayangadw@users.noreply.github.com>
2025-01-16 20:35:07 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,13 @@
+#How to find the average of N numbers34
+
+
+num = int(input("How many numbers are there ? "))
+total_number=0
+for n in range (num):
+    numbers = float(input("Enter the number : "))
+    total_sum =+ numbers
+average=total_sum/num
+
+print("Average is : ",average)
+
+
